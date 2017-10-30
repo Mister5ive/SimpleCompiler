36e7b932a07d47d36322d6e20b9f2d522f93cbcb
Mister5ive <593063075@qq.com>
2017-10-31 00:09:16 +0800

    添加项目文件。

---
+++ SyntaxAnalysis/CompilerDef.h
@@ -0,0 +1,21 @@
+#ifndef  COMPILERDEF_H_
+#define  COMPILERDEF_H_
+
+
+
+typedef struct _Smart_String
+{
+	int size;//length of string
+	int capacity;//the capacity of buffer
+	char *data;
+}m_SmartString;
+
+
+typedef struct _Smart_Array {
+	int size;//length of array
+	int capacity;//the capacity of buffer
+	void **data;
+
+}m_SmartArray;
+#endif // ! COMPILERDEF_H_
+
