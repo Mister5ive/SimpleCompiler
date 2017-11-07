dff3db7bfb999685176b887d13f18a7a0e58dce0
Mister5ive <593063075@qq.com>
2017-11-08 00:07:57 +0800

    init_lex

--- SyntaxAnalysis/CompilerDef.h
+++ SyntaxAnalysis/CompilerDef.h
@@ -8,7 +8,55 @@
 #define CODEC_CHECK_RETURN_ERROR(P)  if(!P){LogError("%s : %s invalid param, line:%d!",__FILE__, __FUNCTION__, __LINE__); return ERROR_INVALID_PARAM;}
 #define ZERO_MEMORY(VAR) {memset(&VAR, 0, sizeof(VAR));}
 
+enum  eTokenCode {
+	TK_PLUS,
+	TK_MINUS,
+	TK_STAR,
+	TK_DIVIDE,
+	TK_MOD,
+	TK_EQ,
+	TK_NEQ,
+	TK_LT,
+	TK_LEQ,
+	TK_GT,
+	TK_GEQ,
+	TK_ASSIGN,
+	TK_EPOINTSTO,
+	TK_DOT,
+	TK_AND,
+	TK_OPENPA,
+	TK_CLOSEPA,
+	TK_OPENBR,
+	TK_CLOSEBR,
+	TK_BEGIN,
+	TK_END,
+	TK_SEMICOLON,
+	TK_COMMA,
+	TK_ELLIPSIS,
+	TK_EOF,
+	TK_CINT,
+	TK_CCHAR,
+	TK_CSTR,
 
+	KW_CHAR,
+	KW_SHORT,
+	KW_INT,
+	KW_VOID,
+	KW_STRUCT,
+
+	KW_IF,
+	KW_ELSE,
+	KW_FOR,
+	KW_CONTINUE,
+	KW_BREAK,
+	KW_RETURN,
+	KW_SIZEOF,
+	KW_ALIGN,
+	KW_CDECL,
+	KW_STDCALL,
+	TK_IDENT 
+
+};
 
 template<typename T>
 inline void safe_release(T **p, bool b_block)
@@ -74,9 +122,11 @@ typedef struct _Smart_Array {
 
 typedef struct _Tk_Word{
 	int tkcode;//word code
-	char *p_word;//word sring
 	struct _Tk_Word *next;//conflict word
-	//struct Symbol *
+	char *p_word;//word sring
+	struct Symbol *sym_struct;
+	struct Symbol *sym_identifier;
+
 }_TkWord;
 
 
--- SyntaxAnalysis/SyntaxStruct.h
+++ SyntaxAnalysis/SyntaxStruct.h
@@ -223,7 +223,57 @@ private:
 		return (hash & 0x7FFFFFFF);
 
 	}
+	void init_lex() {
+		_TkWord *tp;
+		static _TkWord keywords[] = {
+			{ TK_PLUS,	NULL	,"+",	NULL,	NULL },
+			{ TK_MINUS,	NULL	,"-",	NULL,	NULL },
+			{ TK_STAR,	NULL	,"*",	NULL,	NULL },
+			{ TK_DIVIDE,	NULL	,"/",	NULL,	NULL },
+			{ TK_MOD,	NULL	,"%",	NULL,	NULL },
+			{ TK_EQ,		NULL	,"==",	NULL,	NULL },
+			{ TK_NEQ,	NULL	,"!=",	NULL,	NULL },
+			{ TK_LT,		NULL	,"<",	NULL,	NULL },
+			{ TK_LEQ,	NULL	,"<=",	NULL,	NULL },
+			{ TK_GT,		NULL	,">",	NULL,	NULL },
+			{ TK_GEQ,	NULL	,">=",	NULL,	NULL },
+			{ TK_ASSIGN,	NULL	,"=",	NULL,	NULL },
+			{ TK_EPOINTSTO,	NULL	,"->",	NULL,	NULL },
+			{ TK_DOT,	NULL	,".",	NULL,	NULL },
+			{ TK_AND,	NULL	,"&",	NULL,	NULL },
+			{ TK_OPENPA,	NULL	,"(",	NULL,	NULL },
+			{ TK_CLOSEPA,	NULL	,")",	NULL,	NULL },
+			{ TK_OPENBR,	NULL	,"[",	NULL,	NULL },
+			{ TK_CLOSEBR,	NULL	,"]",	NULL,	NULL },
+			{ TK_BEGIN,		NULL	,"{",	NULL,	NULL },
+			{ TK_END,	NULL	,"}",	NULL,	NULL },
+			{ TK_SEMICOLON,		NULL	,";",	NULL,	NULL },
+			{ TK_COMMA,	NULL	,",",	NULL,	NULL },
+			{ TK_ELLIPSIS,		NULL	,"...",	NULL,	NULL },
+			{ TK_EOF,	NULL	,"End_Of_File",	NULL,	NULL },
+			{ TK_CINT,	NULL	,"整形常量",	NULL,	NULL },
+			{ TK_CCHAR,	NULL	,"字符常量",	NULL,	NULL },
+			{ TK_CSTR,	NULL	,"字符串常量",	NULL,	NULL },
 
+			{ KW_CHAR,	NULL	,"char",	NULL,	NULL },
+			{ KW_SHORT,	NULL	,"short",	NULL,	NULL },
+			{ KW_INT,	NULL	,"int",	NULL,	NULL },
+			{ KW_VOID,	NULL	,"void",	NULL,	NULL },
+			{ KW_STRUCT,	NULL	,"struct",	NULL,	NULL },
+
+			{ KW_IF,	NULL	,"if",	NULL,	NULL },
+			{ KW_ELSE,	NULL	,"else",	NULL,	NULL },
+			{ KW_FOR,	NULL	,"for",	NULL,	NULL },
+			{ KW_CONTINUE,	NULL	,"continue",	NULL,	NULL },
+			{ KW_BREAK,	NULL	,"break",	NULL,	NULL },
+			{ KW_RETURN,	NULL	,"return",	NULL,	NULL },
+			{ KW_SIZEOF,	NULL	,"sizeof",	NULL,	NULL },
+			{ KW_ALIGN,	NULL	,"__align",	NULL,	NULL },
+			{ KW_CDECL,	NULL	,"__cdecl",	NULL,	NULL },
+			{ KW_STDCALL,	NULL	,"__stdcall",	NULL,	NULL },
+			{ 0,	NULL	,NULL,	NULL,	NULL },
+		};
+	}
 
 
 };
