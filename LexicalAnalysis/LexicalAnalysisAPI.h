c0e37b5af831fab592bd4934db6d5a1ddebbd554
Mister5ive <593063075@qq.com>
2017-11-20 19:05:06 +0800

    virtual to do

--- LexicalAnalysis/LexicalAnalysisAPI.h
+++ LexicalAnalysis/LexicalAnalysisAPI.h
@@ -3,3 +3,25 @@
 #else
 #define LEXICALANALYSIS_API __declspec(dllimport)
 #endif
+
+class LexicalAnalysis {
+private:
+	void getch();
+	void preprocess();
+	void skip_white_space();
+	void parse_comment();
+	void parse_identifier();
+	void parse_num();
+	void parse_string(char str);
+	void process();
+	void token_colored(int lex_state);
+	char* get_tkstr(int token);
+	void skip(int c);
+public:
+	LexicalAnalysis();
+	~LexicalAnalysis();
+	int init();
+	int open(char* file_name);
+	int close();
+	void run();
+};
